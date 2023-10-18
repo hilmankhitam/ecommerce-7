3c5eb46031b5d2b6639686e07f7af6b4a4d7ddda
hilman khitam <hilmankhitam100@gmail.com>
2023-10-18 12:36:14 +0800

    navigation bar

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation";
 
 import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/Navbar";
 
 export default async function DashboardLayout({ children, params }: { children: React.ReactNode, params: { storeId: string } }) {
     const { userId } = auth();
@@ -23,7 +24,7 @@ export default async function DashboardLayout({ children, params }: { children: 
 
     return (
         <>
-            <div>This will be a Navbar</div>
+            <Navbar />
             {children}
         </>
     )
