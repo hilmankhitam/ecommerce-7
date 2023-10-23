495bcea0fa27ce854d5e7028c46d464131cd6fb3
hilman khitam <hilmankhitam100@gmail.com>
2023-10-23 10:15:04 +0800

    color entity

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Plus } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+import { ColorColumn, columns } from "./columns";
+
+interface ColorClientProps {
+    data: ColorColumn[]
+}
+
+export const BillboardClient: React.FC<ColorClientProps> = ({ data }) => {
+    const params = useParams();
+    const router = useRouter();
+
+
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading title={`Colors (${data.length})`} description="Manage Colors for your store" />
+                <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+                    <Plus className="mr-2 w-4 h-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+            <DataTable searchKey="name" columns={columns} data={data} />
+            <Heading title="API" description="API calls for billboards" />
+            <Separator />
+            <ApiList entityName="colors" entityIdName="colorId"/>
+        </>
+    );
+}
---
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,37 @@
+import prismadb from '@/lib/prismadb'
+import { format } from 'date-fns';
+
+import { BillboardClient } from './components/client'
+import { ColorColumn } from './components/columns';
+
+const ColorPage = async ({
+    params
+}: {
+    params: { storeId: string }
+}) => {
+    const colors = await prismadb.color.findMany({
+        where: {
+            storeId: params.storeId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    });
+
+    const formattedColors: ColorColumn[] = colors.map((item) => ({
+        id: item.id,
+        name: item.name,
+        value: item.value,
+        createdAt: format(item.createdAt, "MMMM do, yyyy")
+    }))
+
+    return (
+        <div className='flex-col'>
+            <div className="flex-1 space-y-4 p-8 pt-4">
+                <BillboardClient data={formattedColors} />
+            </div>
+        </div>
+    )
+}
+
+export default ColorPage
